90fd545c27874545cdaed531c574e44c2bf106f0
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-04 16:34:30 -0500

    Created classes MeleeTaunt, RangedTaunt

---
+++ strategy-pattern/MeleeTaunt.java
@@ -0,0 +1,8 @@
+
+public class MeleeTaunt {
+	
+	void taunt() {
+		System.out.println("Fight me hand to hand!");
+	}
+
+}
---
+++ strategy-pattern/RangedTaunt.java
@@ -0,0 +1,8 @@
+
+public class RangedTaunt {
+	
+	void tuant() {
+		System.out.println("I'll strike you from a distance!");
+	}
+
+}
