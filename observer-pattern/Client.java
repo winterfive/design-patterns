0aff0d621ab14b83b56ddb8ffff6cf34fdd81e53
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-15 16:29:33 -0500

    Updated Client class

--- observer-pattern/Client.java
+++ observer-pattern/Client.java
@@ -2,6 +2,11 @@ public class Client {
 
 	public static void main(String[] args) {
 		
+		Client myClient = new Client();
+		myClient.begin();
+	}
+	
+	public void begin() {
 		RideData rd = new RideData();		
 		rd.setWaitTimes(30, 45, 75);
 	}
