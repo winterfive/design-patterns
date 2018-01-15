8051f70c5ab296e6493f3ac3e5c8cbc685df0c28
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-15 16:22:10 -0500

    Numerous updates and new classes

---
+++ observer-pattern/Client.java
@@ -0,0 +1,9 @@
+public class Client {
+
+	public static void main(String[] args) {
+		RideData rd = new RideData();
+		
+		rd.setWaitTimes(30, 45, 75);
+	}
+
+}
--- observer-pattern/RideData.java
+++ observer-pattern/RideData.java
@@ -1,6 +1,36 @@
+import java.util.Observable;
 
-public class RideData {
-	void getHulkTime() {}
-	void getSpidermanTime() {}
+public class RideData extends Observable{
+	private int hulkWaitTime;
+	private int spidermanWaitTime;
+	private int forbiddenWaitTime;
+	
+	public RideData() {}	
+	
+	public void timeChanged() {
+		setChanged();
+		notifyObservers();
+	}
+		
+	public void setWaitTimes(int hulkWaitTime, int spidermanWaitTime, int forbiddenWaitTime) {
+		this.hulkWaitTime = hulkWaitTime;
+		this.spidermanWaitTime = spidermanWaitTime;
+		this.forbiddenWaitTime = forbiddenWaitTime;
+		timeChanged();
+	}
 
+	public int getHulkWaitTime() {
+		return hulkWaitTime;
+	}
+
+	public int getSpidermanWaitTime() {
+		return spidermanWaitTime;
+	}
+
+	public int getForbiddenWaitTime() {
+		return forbiddenWaitTime;
+	}
+
+	
+	
 }
