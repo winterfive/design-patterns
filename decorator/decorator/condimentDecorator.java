15308ff62ebe0915ec789ddda2f34ae1d9d53a3a
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-23 14:30:09 -0500

    Created condimentDecorator class

---
+++ decorator/decorator/condimentDecorator.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public abstract class condimentDecorator extends Beverage{
+
+	@Override
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public abstract String getDesc();
+
+}
