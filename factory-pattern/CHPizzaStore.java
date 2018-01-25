22827f2e953db99fa0910366042ad628505a629d
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-25 15:36:54 -0500

    Two more PizzaStore subclasses

---
+++ factory-pattern/CHPizzaStore.java
@@ -0,0 +1,21 @@
+
+public class CHPizzaStore {
+	
+	// A PizzaStore franchise
+	
+	public Pizza createPizza(type) {
+		
+		if(item.equals("cheese")) {
+			return new CHCheesePizza();
+		}
+		else if(item.equals("pepperoni")) {
+			return new CHPepperoniPizza();
+		}
+		else if(item.equals("veggie")) {
+			return new CHVeggiePizza();
+		} else {
+			return null;
+		}
+	}
+
+}
---
+++ factory-pattern/NYPizzaStore.java
@@ -0,0 +1,21 @@
+
+public class NYPizzaStore extends PizzaStore {
+	
+	// A PizzaStore franchise
+	
+	public Pizza createPizza(String item) {
+		
+		if(item.equals("cheese")) {
+			return new NYCheesePizza();
+		}
+		else if(item.equals("pepperoni")) {
+			return new NYPepperoniPizza();
+		}
+		else if(item.equals("veggie")) {
+			return new NYVeggiePizza();
+		} else {
+			return null;
+		}
+	}
+
+}
