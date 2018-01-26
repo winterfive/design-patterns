863a70ccd7a9edc06b54f487349c305d975f88ac
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-26 16:46:13 -0500

    Built constructor for CHPepperoniPizza

--- factory-pattern/CHPepperoniPizza.java
+++ factory-pattern/CHPepperoniPizza.java
@@ -1,4 +1,21 @@
 
 public class CHPepperoniPizza extends Pizza {
+	
+	public CHPepperoniPizza() {
+		name = "Chicago Style Pepperoni Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Pplum Tomato Sauce";
+		
+		toppings.add("Shredded Mozarella Cheese");
+		toppings.add("Black Olives");
+		toppings.add("Spinach");
+		toppings.add("Eggplant");
+		toppings.add("Sliced Pepperoni");
+		
+	}
+	
+	void cut() {
+		System.out.println("Cutting the pizza into sqaure slices.");
+	}
 
 }
