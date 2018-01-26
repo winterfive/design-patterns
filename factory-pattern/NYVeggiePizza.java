10593746530a951617ff9eb0f4670831e1313ff8
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-26 16:50:27 -0500

    Created constructor for NYVeggie Pizza

--- factory-pattern/NYVeggiePizza.java
+++ factory-pattern/NYVeggiePizza.java
@@ -1,4 +1,15 @@
 
 public class NYVeggiePizza extends Pizza {
 
+	public NYVeggiePizza() {
+		name = "NY Style Veggie Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+ 
+		toppings.add("Grated Reggiano Cheese");
+		toppings.add("Garlic");
+		toppings.add("Onion");
+		toppings.add("Mushrooms");
+		toppings.add("Red Pepper");
+}
 }
