5210d5eac8643547292f32783be9f03be42aa932
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-26 16:48:35 -0500

    Created constructor for CHVeggiePizza

--- factory-pattern/CHVeggiePizza.java
+++ factory-pattern/CHVeggiePizza.java
@@ -1,4 +1,18 @@
 
 public class CHVeggiePizza extends Pizza {
-
+	
+	public CHVeggiePizza() {
+		name = "Chicago Deep Dish Veggie Pizza";
+		dough = "Extra Thick Crust Dough";
+		sauce = "Plum Tomato Sauce";
+ 
+		toppings.add("Shredded Mozzarella Cheese");
+		toppings.add("Black Olives");
+		toppings.add("Spinach");
+		toppings.add("Eggplant");
+	}
+ 
+	void cut() {
+		System.out.println("Cutting the pizza into square slices");
+	}
 }
