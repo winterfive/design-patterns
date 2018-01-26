87461aba9a3d426b6b61a63f9a7a90e92329f6ad
Lee Gainer <winterfive@users.noreply.github.com>
2018-01-26 16:49:28 -0500

    Created constructor for NYPepperoniPizza

--- factory-pattern/NYPepperoniPizza.java
+++ factory-pattern/NYPepperoniPizza.java
@@ -1,4 +1,17 @@
 
 public class NYPepperoniPizza extends Pizza {
+	
+	public NYPepperoniPizza() {
+		name = "NY Style Pepperoni Pizza";
+		dough = "Thin Crust Dough";
+		sauce = "Marinara Sauce";
+ 
+		toppings.add("Grated Reggiano Cheese");
+		toppings.add("Sliced Pepperoni");
+		toppings.add("Garlic");
+		toppings.add("Onion");
+		toppings.add("Mushrooms");
+		toppings.add("Red Pepper");
+	}
 
 }
