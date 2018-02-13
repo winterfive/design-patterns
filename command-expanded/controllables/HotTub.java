eb939d2208cfe1175ee4a118e1f66bc89e7da825
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-13 16:45:39 -0500

    Created HotTub class

---
+++ command-expanded/controllables/HotTub.java
@@ -0,0 +1,13 @@
+package controllables;
+
+public class HotTub {
+	
+	public void on() {
+		System.out.println("The hot tub is on and it's bubbling.");
+	}
+	
+	public void off() {
+		System.out.println("The hot tub is off. Party's over.");
+	}
+
+}
