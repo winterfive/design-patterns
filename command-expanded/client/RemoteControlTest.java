6bdeb778cc701e5a58761729a84ce5f5772ff39e
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-13 11:40:39 -0500

    Applied generic Light, LightOnCommand(), and LightOffCommand(0 to garage light

--- command-expanded/client/RemoteControlTest.java
+++ command-expanded/client/RemoteControlTest.java
@@ -10,19 +10,19 @@ public class RemoteControlTest {
 		
 		RemoteControl remote = new RemoteControl();
 		
-		Light livingRoomLight = new Light();
-		Light kitchenLight = new Light();		
+		Light livingRoomLight = new Light("living room");
+		Light kitchenLight = new Light("kitchen");		
 		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
 		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);		
 		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
 		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
 		
 		GarageDoor gDoor = new GarageDoor();
-		GarageLight gLight = new GarageLight();
+		Light gLight = new Light("garage");
 		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(gDoor);
 		GarageDoorCloseCommand garageClose = new GarageDoorCloseCommand(gDoor);
-		GarageLightOnCommand garageLightOn = new GarageLightOnCommand(gLight);
-		GarageLightOnCommand garageLightOff = new GarageLightOnCommand(gLight);
+		LightOnCommand garageLightOn = new LightOnCommand(gLight);
+		LightOffCommand garageLightOff = new LightOffCommand(gLight);
 		
 		MusicStream ms = new MusicStream();
 		MusicStreamOnCommand mson = new MusicStreamOnCommand(ms);
@@ -33,6 +33,19 @@ public class RemoteControlTest {
 		remote.setCommand(2, garageOpen, garageClose);
 		remote.setCommand(3, garageLightOn, garageLightOff);
 		remote.setCommand(4, mson, msoff);
+		
+		System.out.println(remote);
+		
+		remote.onButtonWasPushed(0);
+		remote.offButtonWasPushed(0);
+		remote.onButtonWasPushed(1);
+		remote.offButtonWasPushed(1);
+		remote.onButtonWasPushed(2);
+		remote.offButtonWasPushed(2);
+		remote.onButtonWasPushed(3);
+		remote.offButtonWasPushed(3);
+		remote.onButtonWasPushed(4);
+		remote.offButtonWasPushed(4);
 	}
 
 }
