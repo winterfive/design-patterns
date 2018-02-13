c1ad5d2caec35575fa9e2e18db3f6862700fef8c
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-13 17:03:03 -0500

    Created FonduePot class

---
+++ command-expanded/controllables/FonduePot.java
@@ -0,0 +1,14 @@
+package controllables;
+
+public class FonduePot {
+	String cheese;
+	
+	public FonduePot(String ch) {
+		this.cheese = ch;
+	}
+	
+	public void on() {
+		System.out.println("The fondue pot is on and filled with deliscious melted " + cheese + ".");
+	}
+
+}
