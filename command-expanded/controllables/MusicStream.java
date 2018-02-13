dc6246519cd412126234675abea57ce7de1eee62
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-13 14:40:58 -0500

    Moved to Controllables folder

---
+++ command-expanded/controllables/MusicStream.java
@@ -0,0 +1,15 @@
+package controllables;
+
+public class MusicStream {
+
+	public void On() {
+		System.out.println("The music is playing.");
+		
+	}
+
+	public void adjustVolume() {
+		System.out.println("The music is too good. Turn it up!");
+		
+	}
+
+}
