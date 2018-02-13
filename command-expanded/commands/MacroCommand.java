535e10cada19be47644b15107a71271d0bf845b0
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-13 16:41:13 -0500

    Corrected MacroCommand execute()

--- command-expanded/commands/MacroCommand.java
+++ command-expanded/commands/MacroCommand.java
@@ -7,16 +7,14 @@ public class MacroCommand implements Command{
 		this.commands = commands;
 	}
 
-	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void undo() {
 		int size = commands.length;
 		for(int i = 0; i < size; i++) {
 			commands[i].execute();
 		}		
+	}
+
+	public void undo() {
+				
 	}
 }
