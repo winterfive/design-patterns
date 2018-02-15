dbab3de96267f1fe80f45668b5db7daef2debb2b
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-15 16:48:23 -0500

    Copied MusicStream class from command-expanded

---
+++ command-lambdas/MusicStream.java
@@ -0,0 +1,17 @@
+public class MusicStream {
+
+	public void On() {
+		System.out.println("The music is playing.");
+		
+	}
+
+	public void adjustVolume() {
+		System.out.println("The music is really good. Turn it up!");
+		
+	}
+	
+	public void Off() {
+		System.out.println("The music is off.");
+	}
+
+}
