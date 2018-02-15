0404d174ea248aeaee5285daf326480026eb1d59
Lee Gainer <winterfive@users.noreply.github.com>
2018-02-15 16:49:16 -0500

    Added code: method reference and multiple methods in a lambda

--- command-lambdas/RemoteControlTest.java
+++ command-lambdas/RemoteControlTest.java
@@ -7,6 +7,7 @@ public class RemoteControlTest {
 		Light livingRoomLight = new Light("living room");		
 		Light kitchenLight = new Light("kitchen");		
 		Light garageLight = new Light("garage");
+		MusicStream music = new MusicStream();		
 			
 		// Remote
 		
@@ -14,6 +15,11 @@ public class RemoteControlTest {
 		remote.setCommand(1, () -> { kitchenLight.on(); }, () -> { kitchenLight.off(); });
 		remote.setCommand(2, () -> { garageLight.on(); }, () -> { garageLight.off(); });
 		remote.setCommand(3, livingRoomLight::on,  livingRoomLight::off); 	// method references
+		
+		// Lambda w/ multiple methods
+		Command musicStreamOn = () -> { music.On(); music.adjustVolume(); };
+		
+		remote.setCommand(4, musicStreamOn, music::Off);
 		// Remaining slots have noCommand via RemoteControl() constructor
 		
 		System.out.println(remote);
@@ -27,5 +33,7 @@ public class RemoteControlTest {
 		remote.offButtonWasPushed(2);
 		remote.onButtonWasPushed(3);
 		remote.offButtonWasPushed(3);
+		remote.onButtonWasPushed(4);
+		remote.offButtonWasPushed(4);
 	}
 }
