b1261bdaa459e83d5539c3e68f3d8980f3f7ea37
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-01 13:47:41 -0500

    Added on() and off()

--- facade-pattern/defualt/Lights.java
+++ facade-pattern/defualt/Lights.java
@@ -1,5 +1,13 @@
 package defualt;
 
 public class Lights {
+	
+	void on() {
+		System.out.println("Turning the lights up.");
+	}
+	
+	void off() {
+		System.out.println("Turning the lights down.");
+	}
 
 }
