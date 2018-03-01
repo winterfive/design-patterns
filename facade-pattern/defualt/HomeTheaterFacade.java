7f5f78bd454b8c1e0cdb5a6854c40fab633fe061
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-01 16:00:46 -0500

    Changed methods to turnOn() and turnOff()

--- facade-pattern/defualt/HomeTheaterFacade.java
+++ facade-pattern/defualt/HomeTheaterFacade.java
@@ -1,40 +1,14 @@
 package defualt;
 
 public class HomeTheaterFacade {
-	SoundSystem sound;
-	Projector projector;
-	Lights lights;
-	Screen screen;
-	PopcornPopper popper;
+	HomeTheater ht;
 	
-	public HomeTheaterFacade(SoundSystem sound,
-			Projector projector,
-			Lights lights,
-			Screen screen,
-			PopcornPopper popper) {
-		
-		this.sound = sound;
-		this.projector = projector;
-		this.lights = lights;
-		this.screen = screen;
-		this.popper = popper;		
+	void on(String movie, int volume) {
+		ht.turnOn(movie, volume);
 	}
 	
-	public void netflixAndChill(String title) {
-		System.out.println("Get ready to watch some Netflix...");
-		popper.on();
-		sound.on();
-		sound.setVolume(7);
-		lights.off();
-		screen.down();
-		projector.on();		
-	}
+	void off() {
+		ht.turnOff();
+	}	
 	
-	public void endWatching() {
-		popper.off();
-		sound.off();
-		lights.on();
-		projector.off();
-		screen.up();
-	}
 }
