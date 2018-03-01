e1dc9d93702c3d5b08b498bbf1efacf0da63bfc5
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-01 13:16:09 -0500

    Added on(), off(), and setVolume()

--- facade-pattern/defualt/SoundSystem.java
+++ facade-pattern/defualt/SoundSystem.java
@@ -1,5 +1,17 @@
 package defualt;
 
 public class SoundSystem {
+	
+	void on() {
+		System.out.println("Turning on the sound system.");
+	}
+	
+	void off() {
+		System.out.println("Turning the sound system off.");
+	}
+	
+	void setVolume(int volume) {
+		System.out.println("Setting volume to " + volume + ".");
+	}
 
 }
