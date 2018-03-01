cdcdea72fd848bccd8e8a8432c05e39d4c7f47dd
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-01 16:34:51 -0500

    Changed on(), off() to turnOn(), turnOff()

---
+++ facade-pattern/defualt/Test.java
@@ -0,0 +1,13 @@
+package defualt;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		HomeTheaterFacade htf = new HomeTheaterFacade();
+		
+		htf.turnOn("Amelie", 7);
+		htf.turnOff();
+	}
+
+}
