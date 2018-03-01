bdcc51f5d6ad1fe728a00bab4dca409bfa7efd3e
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-01 13:49:44 -0500

    Added on() and off()

--- facade-pattern/defualt/PopcornPopper.java
+++ facade-pattern/defualt/PopcornPopper.java
@@ -1,5 +1,13 @@
 package defualt;
 
 public class PopcornPopper {
+	
+	void on() {
+		System.out.println("Can't you smell the popcorn popping?");
+	}
+	
+	void off() {
+		System.out.println("The popper is off.");
+	}
 
 }
