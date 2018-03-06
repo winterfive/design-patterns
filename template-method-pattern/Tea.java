341f6b1522801835901960e9b0aeb7f155721140
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-06 15:18:42 -0500

    Created Tea class

---
+++ template-method-pattern/Tea.java
@@ -0,0 +1,32 @@
+
+public class Tea {
+	
+	void prepareRecipe() {
+		boilWater();
+		steepTeaBag();
+		pourInCup();
+		addLemon();
+	}
+
+	private void boilWater() {
+		System.out.println("Boiling water.");
+		
+	}
+
+	private void addLemon() {
+		System.out.println("Adding lemon.");
+		
+	}
+
+	private void pourInCup() {
+		System.out.println("Pouring into a cup");
+		
+	}
+
+	private void steepTeaBag() {
+		System.out.println("Steeping a teabag.");
+		
+	}
+	
+
+}
