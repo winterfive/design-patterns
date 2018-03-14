078d5eea694b6c08802d741a60aeede254b99884
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-14 16:20:10 -0400

    Added customerWantsCondiments method

--- template-method-pattern/Coffee.java
+++ template-method-pattern/Coffee.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Coffee extends Beverage {
 
@@ -7,6 +10,36 @@ public class Coffee extends Beverage {
 
 	void brew() {
 		System.out.println("Dripping coffee through filter.");
+	}
+	
+	public boolean customerWantsCondiments() {
+		
+		String answer = getUserInput();
+		
+		if(answer.toLowerCase().startsWith("y")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private String getUserInput() {
+		
+		String answer = null;
+		
+		System.out.println("Would you like milk or sugar in your coffee? (y/n)");
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			answer = br.readLine();
+		} catch (IOException ioe) {
+			System.err.print("IO error trying to read your answer.");
+		}
+		
+		if(answer == null) {
+			return "no";
+		}
+		return answer;
 	}
 
 }
