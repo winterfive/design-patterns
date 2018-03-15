310558c8f07db5ec33e67b33d1960a77fcde1a02
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-14 20:05:28 -0400

    Created template-method example with Swing

---
+++ template-swing/MyFrame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+
+	public static void main(String[] args) {
+		
+		public MyFrame(String title) {
+			super(title);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			
+			this.setSize(300, 300);
+			this.setVisible(true);
+		}
+
+	}
+
+}
