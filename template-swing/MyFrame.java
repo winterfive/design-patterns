1cb344788094a1f3a0f9079119011d59040af861
Lee Gainer <winterfive@users.noreply.github.com>
2018-03-14 20:13:12 -0400

    Created template-method example w/ Swing

--- template-swing/MyFrame.java
+++ template-swing/MyFrame.java
@@ -1,16 +1,27 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame{
-
-	public static void main(String[] args) {
 		
-		public MyFrame(String title) {
-			super(title);
-			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			
-			this.setSize(300, 300);
-			this.setVisible(true);
-		}
-
+	public MyFrame(String title) {
+		super(title);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			
+		this.setSize(300, 300);
+		this.setVisible(true);
+		this.setForeground(Color.red);
+		this.getContentPane().setBackground(Color.blue);
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		String msg = "Yes, have some!";
+		g.drawString(msg, 100, 100);		
+	}
+	
+	public static void main(String[] args) {
+		MyFrame mf = new MyFrame("Vinz Clortho says...");
 	}
 
 }
