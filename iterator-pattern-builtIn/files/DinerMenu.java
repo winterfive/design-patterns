955ed6f43d307dba86c3ee572e7c079881e7670a
Lee Gainer <winterfive@users.noreply.github.com>
2018-04-03 10:31:04 -1000

    Implemented Menu inteface

--- iterator-pattern-builtIn/files/DinerMenu.java
+++ iterator-pattern-builtIn/files/DinerMenu.java
@@ -1,8 +1,6 @@
 package files;
 
-import java.util.Iterator;
-
-public class DinerMenu {
+public class DinerMenu implements Menu{
 	static final int MAX_ITEMS = 6;
 	int numberOfItems = 0;
 	MenuItem[] menuItems;
